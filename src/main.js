d67fcff7df7aae43e5176ed66340d9d6df3d9967
gaoly <shuiruohanyu@foxmail.com>
2019-08-31 10:40:59 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
 new Vue({
