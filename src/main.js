9e018ce1064f1de0abe5bd8c54c90d73362a3086
gaoly <shuiruohanyu@foxmail.com>
2019-08-31 16:22:03 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './style/index.less' // 引入初始化样式 包括 margin padding li
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 赋值基础地址
+Vue.prototype.$axios = axios // 给Vue对象的原型属性赋值 那么所有vue实例自动拥有$axios
+
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
 new Vue({
