c009fd235ee57acd7d77a1d78c3b2e4bbd48aa7c
gaoly <shuiruohanyu@foxmail.com>
2019-08-31 10:26:17 +0800

    初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
 new Vue({
   router,
   render: h => h(App)
