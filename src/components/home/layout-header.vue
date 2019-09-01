80012567fa15e34a8aadb5dfbcc7a0a98130257e
gaoly <shuiruohanyu@foxmail.com>
2019-09-01 14:46:41 +0800

    黑马头条PC-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -9,16 +9,16 @@
       <el-col :span="3" class='right'>
         <!-- 属性不给:就相当于字符串 -->
           <img class='head-img' :src="userInfo.photo ? userInfo.photo : defaultImg" alt="">
-     <el-dropdown trigger="click">
+     <el-dropdown trigger="click"  @command="handleMenuItem">
          <!-- 匿名插槽 -->
        <span class="el-dropdown-link">
           {{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i>
        </span>
        <!-- 具名插槽 -->
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item >个人信息</el-dropdown-item>
-        <el-dropdown-item>git地址</el-dropdown-item>
-        <el-dropdown-item>退出</el-dropdown-item>
+        <el-dropdown-item command='account'>个人信息</el-dropdown-item>
+        <el-dropdown-item command='git'>git地址</el-dropdown-item>
+        <el-dropdown-item command='lgout'>退出</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
       </el-col>
@@ -43,6 +43,19 @@ export default {
       }).then(result => {
         this.userInfo = result.data.data
       })
+    },
+    // 点击菜单项触发的方法
+    handleMenuItem (command) {
+      if (command === 'account') {
+        // 账户信息
+      } else if (command === 'git') {
+        // git地址
+        window.location.href = 'https://github.com/shuiruohanyu/82heimatoutiao' // 改变当前的地址
+      } else {
+        // 退出
+        window.localStorage.clear() // 清空缓存 清除所有的缓存  只能清除自己当前项目的缓存
+        this.$router.push('/login') // 跳转到登录页
+      }
     }
   },
   created () {
