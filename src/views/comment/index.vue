85ad1e538a22eb9ae8a111035463b97f51a87281
gaoly <shuiruohanyu@foxmail.com>
2019-09-03 15:54:29 +0800

    黑马头条PC-评论列表-列表分页及请求

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,26 +1,35 @@
 <template>
-<!-- 最外层 用el-card卡片做页面 -->
+  <!-- 最外层 用el-card卡片做页面 -->
   <el-card>
     <!-- 插槽内容 => 标题 -->
-    <bread-crumb slot='header'>
-     <!-- 面包屑的具名插槽 -->
-      <template slot='title'>评论列表</template>
+    <bread-crumb slot="header">
+      <!-- 面包屑的具名插槽 -->
+      <template slot="title">评论列表</template>
     </bread-crumb>
     <!-- 表格 需要数据 :data-->
     <el-table :data="list">
       <!-- formatter是el-table-column属性 -->
-      <el-table-column  width="700" label="标题" prop='title'></el-table-column>
-      <el-table-column :formatter="formatter" label="评论状态" prop='comment_status'></el-table-column>
-      <el-table-column label="总评论数" prop='total_comment_count'></el-table-column>
-      <el-table-column label="粉丝评论数" prop='fans_comment_count'></el-table-column>
-      <el-table-column label="操作" >
+      <el-table-column width="700" label="标题" prop="title"></el-table-column>
+      <el-table-column :formatter="formatter" label="评论状态" prop="comment_status"></el-table-column>
+      <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+      <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+      <el-table-column label="操作">
         <!-- 作用域插槽 slot-scope="变量" 变量  就是 row,column,$index.store的属性集合 obj.row -->
         <template slot-scope="obj">
-         <el-button size="small" type="text">修改评论</el-button>
-         <el-button @click="openOrClose(obj.row)" size="small" :style='{color: obj.row.comment_status ? "#E6A23C" :"#409EFF"}' type="text">{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
+          <el-button size="small" type="text">修改评论</el-button>
+          <el-button
+            @click="openOrClose(obj.row)"
+            size="small"
+            :style="{color: obj.row.comment_status ? '#E6A23C' : '#409EFF'}"
+            type="text"
+          >{{obj.row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center" style='margin:10px 0'>
+      <!-- 分页组件  current-page当前页码 每页显示多少条 page-size total 总数 -->
+      <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize" :total="page.total" background layout="prev, pager, next" ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -28,10 +37,21 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        page: 1, // 当前页码
+        pageSize: 10, // 当前每页条数
+        total: 0 // 总条数
+      }
+
     }
   },
   methods: {
+    changePage (newPage) {
+      // 给当前页码更新最新值
+      this.page.page = newPage
+      this.getComments() // 获取最新页码的数据
+    },
     // 打开或者关闭评论
     openOrClose (row) {
       let mess = row.comment_status ? '关闭' : '打开'
@@ -55,9 +75,10 @@ export default {
       // body 路径参数  data
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.page, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -68,5 +89,4 @@ export default {
 </script>
 
 <style>
-
 </style>
