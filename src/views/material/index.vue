bedca43eb00385419af0132e75f778753f0edcad
gaoly <shuiruohanyu@foxmail.com>
2019-09-03 17:31:11 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,28 +1,34 @@
 <template>
-  <el-card class='material'>
+  <el-card class="material">
     <bread-crumb slot="header">
       <template slot="title">素材管理</template>
     </bread-crumb>
     <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
         <!-- 全部素材的内容 -->
-        <div class='card-list'>
-          <el-card class='img-card' v-for="item in list" :key="item.id">
+        <div class="card-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
-            <el-row class='operate' type='flex' align="middle" justify="space-around">
-                <i :style="{color: item.is_collected ? 'red' : '' }" class='el-icon-star-on'></i>
-                <i class='el-icon-delete-solid'></i>
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i :style="{color: item.is_collected ? 'red' : '' }" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
             </el-row>
           </el-card>
         </div>
+        <el-row type='flex' justify="center">
+          <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize"  :total="page.total" background layout="prev, pager, next" ></el-pagination>
+        </el-row>
       </el-tab-pane>
       <el-tab-pane label="收藏图片" name="collect">
         <!-- 收藏素材的内容 -->
-        <div class='card-list'>
-          <el-card class='img-card' v-for="item in list" :key="item.id">
+        <div class="card-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
           </el-card>
         </div>
+         <el-row type='flex' justify="center">
+          <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize"  :total="page.total" background layout="prev, pager, next" ></el-pagination>
+        </el-row>
       </el-tab-pane>
     </el-tabs>
   </el-card>
@@ -33,26 +39,39 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        page: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.page = newPage
+      this.getMaterial() // 请求最新的数据
+    },
     //   切换页签
     changeTab () {
       // this.activeName是最新的页签
       // 加载不同类型的数据
       // all => 所有的数据
       // collect => 去加载收藏数据
+      this.page.page = 1
       this.getMaterial()
     },
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.page, // 默认第一次是第一页
+          per_page: this.page.pageSize, // 默认pageSize 10条
           collect: this.activeName === 'collect' // collect为false就是查全部数据 collect 为true的话 是查询收藏数据
         }
       }).then(result => {
         this.list = result.data.results // 接收数据
+        this.page.total = result.data.total_count // 将图片总数赋值给总页码
       })
     }
   },
@@ -64,29 +83,29 @@ export default {
 
 <style lang='less' scoped>
 .material {
-    .card-list {
-     display: flex;
-     flex-wrap: wrap;
-     justify-content: center;
-     .img-card {
-         width: 180px;
-         height:180px;
-         margin:30px;
-         position: relative;
-         img {
-             width:100%;
-             height:100%;
-         }
-         .operate {
-             position: absolute;
-             width:100%;
-             bottom:0;
-             left:0;
-             height:30px;
-             background-color: #f4f5f6;
-             font-size:18px;
-         }
-     }
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .img-card {
+      width: 180px;
+      height: 180px;
+      margin: 30px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+        height: 30px;
+        background-color: #f4f5f6;
+        font-size: 18px;
+      }
     }
+  }
 }
 </style>
