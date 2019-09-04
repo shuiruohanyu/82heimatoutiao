2038d6acfacc34e3afadfd05b34ed308bc8e4e2d
gaoly <shuiruohanyu@foxmail.com>
2019-09-04 10:48:09 +0800

    内容列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -31,6 +31,11 @@ export default new Router({
       {
         path: 'material',
         component: () => import('./views/material')
+      },
+      {
+        // 内容管理
+        path: 'articles',
+        component: () => import('./views/articles')
       }
       ]
     }
---
+++ src/views/articles/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-card>
+      <bread-crumb slot='header'>
+       <template slot='title'>内容管理</template>
+      </bread-crumb>
+  </el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
