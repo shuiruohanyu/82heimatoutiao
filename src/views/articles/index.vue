8df65e28545e388aedfaad70ae7e93e74e018bb6
gaoly <shuiruohanyu@foxmail.com>
2019-09-04 15:22:22 +0800

    黑马头条PC-内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -5,49 +5,56 @@
     </bread-crumb>
     <!-- 搜索工具栏 -->
     <el-form style="margin-left:40px">
-      <el-form-item label='文章状态:'>
-          <el-radio-group>
-              <el-radio>全部</el-radio>
-              <el-radio>草稿</el-radio>
-              <el-radio>待审核</el-radio>
-              <el-radio>审核通过</el-radio>
-              <el-radio>审核失败</el-radio>
-          </el-radio-group>
+      <el-form-item label="文章状态:">
+        <!-- 文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部 -->
+        <el-radio-group @change="changeCondition" v-model="searchForm.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表:">
-          <el-select></el-select>
+        <el-select @change="changeCondition" v-model="searchForm.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="时间选择:">
-          <el-date-picker
-            type="datetimerange"
-            range-separator="至"
-            start-placeholder="开始日期"
-           end-placeholder="结束日期">
-         </el-date-picker>
+        <el-date-picker
+         @change="changeCondition"
+          value-format="yyyy-MM-dd"
+          v-model="searchForm.dateRange"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
     </el-form>
     <!-- 内容页面结构 -->
-    <div class='total-info'>
-        共找到1459条符合条件的内容
-    </div>
-    <div class='article-list'>
-        <!-- 循环项 -->
-        <div class='article-item' v-for="(item,index) in list" :key="index">
-            <!-- 左侧内容 -->
-            <div class='left'>
-                <img :src="item.cover.images.length ? item.cover.images[0] : defaultImg" alt="">
-                <div class='info'>
-                    <span class='title'>{{item.title}}</span>
-                    <el-tag style='width:80px'>{{item.status | statusText}}</el-tag>
-                    <span class='date'>{{item.pubdate}}</span>
-                </div>
-            </div>
-            <!-- 右侧内容 -->
-            <div class='right'>
-                <span><i class='el-icon-edit'></i>修改</span>
-                <span><i class='el-icon-delete'></i>删除</span>
-            </div>
+    <div class="total-info">共找到1459条符合条件的内容</div>
+    <div class="article-list">
+      <!-- 循环项 -->
+      <div class="article-item" v-for="(item,index) in list" :key="index">
+        <!-- 左侧内容 -->
+        <div class="left">
+          <img :src="item.cover.images.length ? item.cover.images[0] : defaultImg" alt />
+          <div class="info">
+            <span class="title">{{item.title}}</span>
+            <el-tag style="width:80px">{{item.status | statusText}}</el-tag>
+            <span class="date">{{item.pubdate}}</span>
+          </div>
         </div>
+        <!-- 右侧内容 -->
+        <div class="right">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i>删除
+          </span>
+        </div>
+      </div>
     </div>
   </el-card>
 </template>
@@ -57,20 +64,46 @@ export default {
   data () {
     return {
       list: [], // 定义一个空数组
-      defaultImg: require('../../assets/img/default-cover.jpg') // base64字符串
+      defaultImg: require('../../assets/img/default-cover.jpg'), // base64字符串
+      searchForm: {
+        //   定义一个表单数据对象
+        status: 5, // 状态
+        channel_id: null,
+        dateRange: [] // 数组 [开始时间,结束时间][1,2]
+      },
+      channels: [] // 频道列表数据
     }
   },
   methods: {
-    getArticles () {
+    changeCondition () {
+      let params = {
+        status: this.searchForm.status === 5 ? null : this.searchForm.status,
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate: this.searchForm.dateRange.length > 0 ? this.searchForm.dateRange[0] : null,
+        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null
+      }
+      this.getArticles(params)
+    },
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(result => {
         this.list = result.data.results
+      })
+    },
+    // 获取频道数据
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
       })
     }
   },
   created () {
     this.getArticles()
+    this.getChannels() // 获取频道数据
   },
   filters: {
     //   定义一个过滤器 过滤状态
@@ -94,48 +127,48 @@ export default {
 
 <style lang='less' scoped>
 .total-info {
-    border-bottom: 1px dashed #CCC;
-    padding: 15px 0;
-    color:#323745;
-    font-size:14px;
+  border-bottom: 1px dashed #ccc;
+  padding: 15px 0;
+  color: #323745;
+  font-size: 14px;
 }
 .article-list {
   .article-item {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 0;
+    border-bottom: 1px solid #f2f3f5;
+    .left {
       display: flex;
-      justify-content: space-between;
-      padding: 15px 0;
-      border-bottom: 1px solid #f2f3f5;
-      .left {
-          display: flex;
-          img {
-              width: 180px;
-              height:100px;
-              border-radius: 4px;
-          }
-          .info {
-              height: 100px;
-              display: flex;
-              flex-direction: column;
-              margin-left:10px;
-              justify-content: space-around;
-              .title {
-                  color: #333;
-                  font-size:14px;
-              }
-              .date {
-                  color:#999;
-                  font-size:12px;
-              }
-          }
+      img {
+        width: 180px;
+        height: 100px;
+        border-radius: 4px;
       }
-      .right {
-          font-size:12px;
-          span {
-              margin-right:8px;
-              cursor: pointer;
-              color:#333;
-          }
+      .info {
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        margin-left: 10px;
+        justify-content: space-around;
+        .title {
+          color: #333;
+          font-size: 14px;
+        }
+        .date {
+          color: #999;
+          font-size: 12px;
+        }
       }
+    }
+    .right {
+      font-size: 12px;
+      span {
+        margin-right: 8px;
+        cursor: pointer;
+        color: #333;
+      }
+    }
   }
 }
 </style>
